82756b2723ec0bab2d9d2878a324f84dd1eaf9df
Justin Seaward <jut_seaward@yahoo.com>
2026-03-24 16:06:49 -0230

    Databaseconnection: added logic to connet to a postgres database

---
+++ src/DBConnection/Databaseconnection.java
@@ -0,0 +1,30 @@
+package DBConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Databaseconnection {
+    private static final String url = "jdbc:postgresql://localhost:5432/qap4";
+    private static final String user = "admin";
+    private static final String password = "password";
+    public static Connection getcon()
+    {
+        Connection connection = null;
+        try{
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Database connection successful");
+        }
+        catch(ClassNotFoundException | SQLException e)
+        {
+            System.out.println("Database connection failed");
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
+
+
+
