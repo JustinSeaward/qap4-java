b647d019e15bbcdf86747f3a2335e49f8c3f1094
Justin Seaward <jut_seaward@yahoo.com>
2026-03-25 00:10:11 -0230

    ReadFile: added logic to read data from files. added readFromTextFile() method to read data from a text file

--- src/FileService/ReadFile.java
+++ src/FileService/ReadFile.java
@@ -1,4 +1,20 @@
 package FileService;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+
 public class ReadFile {
+    public void readFromTextFile(String fileName){
+        try{
+            FileReader fr = new FileReader(fileName + ".txt");
+            System.out.println(fr.readAllAsString());
+            fr.close();
+        }
+        catch(IOException e) {
+            System.out.println("File with that name not found");
+        }
+    }
+
 }
