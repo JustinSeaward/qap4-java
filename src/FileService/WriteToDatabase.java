dab59e678ad64d34c367e6723acc92910ce3022d
Justin Seaward <jut_seaward@yahoo.com>
2026-03-25 20:19:28 -0230

    WriteToDatabase: added logic to write data to a database

--- src/FileService/WriteToDatabase.java
+++ src/FileService/WriteToDatabase.java
@@ -1,10 +1,26 @@
-//package FileService;
-//
-//import EntityClasses.Drug;
-//
-//import java.util.ArrayList;
-//
-//public class WriteToDatabase() {
-//    ArrayList<Drug> drugList = new ArrayList<Drug>();
-//
-//}
+package FileService;
+
+import EntityClasses.Drug;
+import DBConnection.Databaseconnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class WriteToDatabase {
+
+    public void writeDataToDatabase(Drug drug){
+        String query = "INSERT INTO Drugs (name, cost, dosage)" + "VALUES (?,?,?)";
+        try {
+            Connection con = Databaseconnection.getcon();
+            PreparedStatement statement = con.prepareStatement(query);
+            statement.setString(1, drug.getDrugName());
+            statement.setDouble(2, drug.getDrugCost());
+            statement.setString(3, drug.getDosage());
+            int updateRow = statement.executeUpdate();
+            System.out.println(updateRow);
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
