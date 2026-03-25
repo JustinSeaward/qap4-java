32ea19bd137054650dce6221b5341c6ab28292eb
Justin Seaward <jut_seaward@yahoo.com>
2026-03-24 21:35:42 -0230

    WriteFile: added writeToTextFile method with logic to write text to a file

--- src/FileService/WriteFile.java
+++ src/FileService/WriteFile.java
@@ -1,9 +1,19 @@
 package FileService;
 
+import EntityClasses.Patient;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class WriteFile {
 
-    public WriteFile(String userInput){
-
+    public void writeToTextFile(String fileName, Patient patient){
+        try{
+            FileWriter fw = new FileWriter(fileName);
+            fw.write(patient.toString());
+            fw.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
     }
 }
